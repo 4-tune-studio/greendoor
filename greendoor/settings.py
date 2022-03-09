0b77e7b6fc6188bbe0faf2419cef3477fcef869c
KIMHOONHEE <nmdkims@gmail.com>
2022-03-10 01:36:16 +0900

    수정 - 사용 DB sqlLite -> AWS RDS로 변경

--- greendoor/settings.py
+++ greendoor/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+from .my_settings import MY_SECRET, MY_DATABASES
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zhi_y6fvy1nv=*d8%tqdi&ac=*isw41d-g16e&@_jvxros7j=a'
+# SECRET_KEY = 'django-insecure-zhi_y6fvy1nv=*d8%tqdi&ac=*isw41d-g16e&@_jvxros7j=a'
+SECRET_KEY = MY_SECRET['SECRET_KEY']
+DATABASES = MY_DATABASES
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +75,12 @@ WSGI_APPLICATION = 'greendoor.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
