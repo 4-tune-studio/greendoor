5a39dee3367de07219f760c7011124ca756f01da
kkitokki <kyaxxt@gmail.com>
2022-03-10 16:59:27 +0900

    update test

---
+++ order/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class OrderBasket(BaseModel):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="order_basket", db_column="user_id")
+    product_id = models.ForeignKey(PlantCategory, on_delete=models.CASCADE, related_name="order_basket", db_column="product_id")
+    qty = models.IntegerField()
+
+    class Meta:
+        db_table = "order_basket"
+
+
+class Order(BaseModel):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="order", db_column="user_id")
+    status = models.IntegerField(default=2) # 처리중-2, 완료-1, 실패-0
+
+    class Meta:
+        db_table = "order"
+
+
+class OderProduct(BaseModel):
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order_product", db_column="order_id")
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="order_product", db_column="product_id")
+    qty = models.IntegerField()
+
+    class Meta:
+        db_table = "order_product"
