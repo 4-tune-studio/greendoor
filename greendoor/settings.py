1f5e859c5118949017040ceb9993dd2a8e644dd1
Johnny Lee <lowkey4721@gmail.com>
2022-03-10 16:53:16 +0900

    추가 -user app

--- greendoor/settings.py
+++ greendoor/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "user.Users"
