a05a5778799918031760766c441c75bfbf3b0ee0
kkitokki <kyaxxt@gmail.com>
2022-03-24 17:42:00 +0900

    commit

--- greendoor/settings.py
+++ greendoor/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # base_dir은 프로젝트 폴더
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
