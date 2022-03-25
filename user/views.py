70f7a72c34f225e4c839c2d9d6255c3ccd780bb9
KooHyunJung <ghj3160@gmail.com>
2022-03-25 11:27:05 +0900

    추가, 수정 - 프로필 변경 필요 코드 추가/모델 코드 수정

--- user/views.py
+++ user/views.py
@@ -1,9 +1,61 @@
+from django.contrib import auth
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from user.forms import CustomUserChangeForm
+from user.models import Users
 
 # Create your views here.
+
+
+def sign_up_view(request):
+    if request.method == "GET":
+        user = request.user.is_authenticated  # 로그인 된 사용자가 요청하는지 검사
+        if user:  # 로그인이 되어있다면
+            return redirect("/")
+        else:  # 로그인이 되어있지 않다면
+            return render(request, "user/signup.html")
+    elif request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        password2 = request.POST.get("password2", None)
+
+        if password != password2:
+            return render(request, "user/signup.html")
+        else:
+            exist_user = get_user_model().objects.filter(username=username)
+            if exist_user:
+                return render(request, "user/signup.html")  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
+            else:
+                UserModel.objects.create_user(username=username, password=password)
+                return redirect("/sign-in")  # 회원가입이 완료되었으므로 로그인 페이지로 이동
+
+
+def sign_in_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+
+        me = auth.authenticate(request, username=username, password=password)  # 사용자 불러오기
+        if me is not None:  # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
+            auth.login(request, me)
+            return redirect("/")
+        else:
+            return redirect("/sign-in")  # 로그인 실패
+    elif request.method == "GET":
+        user = request.user.is_authenticated  # 사용자가 로그인 되어 있는지 검사
+        if user:  # 로그인이 되어 있다면
+            return redirect("/")
+        else:  # 로그인이 되어 있지 않다면
+            return render(request, "user/signin.html")
+
+
+def logout(request):
+    auth.logout(request)  # 인증 되어있는 정보를 없애기
+    return redirect("/")
+
 
 # =============== 장고 인증 URL + 템플릿 연결 함수 ================ #
 # def accounts_login(request):
@@ -11,59 +63,7 @@ from user.forms import CustomUserChangeForm
 #         return render(request, 'user/signin.html')
 
 
-from django.shortcuts import render, redirect
-from .models import UserModel
-from django.http import HttpResponse
-from django.contrib.auth import get_user_model
-from django.contrib import auth
-from django.contrib.auth.decorators import login_required
-
-
-def sign_up_view(request):
-    if request.method == 'GET':
-        user = request.user.is_authenticated # 로그인 된 사용자가 요청하는지 검사
-        if user: # 로그인이 되어있다면
-            return redirect('/')
-        else: # 로그인이 되어있지 않다면
-            return render(request, 'user/signup.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        password2 = request.POST.get('password2', None)
-
-        if password != password2:
-            return render(request, 'user/signup.html')
-        else:
-            exist_user = get_user_model().objects.filter(username=username)
-            if exist_user:
-                return render(request, 'user/signup.html') # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
-            else:
-                UserModel.objects.create_user(username=username, password=password)
-                return redirect('/sign-in') # 회원가입이 완료되었으므로 로그인 페이지로 이동
-
-def sign_in_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-
-        me = auth.authenticate(request, username=username, password=password)  # 사용자 불러오기
-        if me is not None:  # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
-            auth.login(request, me)
-            return redirect('/')
-        else:
-            return redirect('/sign-in')  # 로그인 실패
-    elif request.method == 'GET':
-        user = request.user.is_authenticated  # 사용자가 로그인 되어 있는지 검사
-        if user:  # 로그인이 되어 있다면
-            return redirect('/')
-        else:  # 로그인이 되어 있지 않다면
-            return render(request, 'user/signin.html')
-
-def logout(request):
-    auth.logout(request) # 인증 되어있는 정보를 없애기
-    return redirect("/")
-
-
+# =============== user profile update ================ #
 # user 프로필 update
 @login_required(login_url="signin")
 def edit(request, pk):
@@ -80,11 +80,10 @@ def edit(request, pk):
     else:
         form = CustomUserChangeForm(instance=request.user)
     context = {"form": form}
-    # 관련 템플릿에 기존 정보를 넘겨 준다
-    return render(request, "tweet/edit.html", context)
+    # 관련 templates 기존 정보를 넘겨 준다
+    return render(request, "user_test/edit.html", context)  # TODO 템플릿 변경시 경로 변경하기
 
 
-# user profile update 페이지 /password_reset/ url 연결
+# user profile update 페이지 -> /password_reset/ url 연결
 def password(request):
     return redirect("/password_reset/")
-
