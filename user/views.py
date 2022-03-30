b0f6f7b4ead06f0d62da0e4a585a80759acf13c3
KooHyunJung <ghj3160@gmail.com>
2022-03-30 11:55:51 +0900

    수정 - user url 변경, 코드 정리

--- user/views.py
+++ user/views.py
@@ -32,7 +32,7 @@ def sign_up_view(request: HttpRequest) -> HttpResponse:
             if exist_user:
                 return render(request, "user/signup.html")  # 사용자가 존재하기 때문에 사용자를 저장하지 않고 회원가입 페이지를 다시 띄움
             else:
-                Users.objects.create_user(username=username, password=password)
+                Users.objects.create_user(nickname=username, password=password)
                 return redirect("/sign-in")  # 회원가입이 완료되었으므로 로그인 페이지로 이동
     else:
         return redirect("/")
